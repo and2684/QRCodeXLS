8f34059cd078b59437b7689581eff57f05e841d9
and2684 <aaaa6226@gmail.com>
2022-10-28 10:51:55 +0500

    Добавьте файлы проекта.

---
+++ QRCodeDesktop.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace QRCodeDesktop
+{
+    partial class QRCodeDesktop
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QRCodeDesktop));
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pbQR = new System.Windows.Forms.PictureBox();
+            this.textQR = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(235, 243);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(108, 24);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Генерировать QR";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(349, 243);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(110, 24);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Сохранить файл";
+            this.btnSave.UseCompatibleTextRendering = true;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pbQR
+            // 
+            this.pbQR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbQR.InitialImage = null;
+            this.pbQR.Location = new System.Drawing.Point(235, 12);
+            this.pbQR.Name = "pbQR";
+            this.pbQR.Size = new System.Drawing.Size(225, 225);
+            this.pbQR.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbQR.TabIndex = 2;
+            this.pbQR.TabStop = false;
+            // 
+            // textQR
+            // 
+            this.textQR.Location = new System.Drawing.Point(12, 35);
+            this.textQR.Name = "textQR";
+            this.textQR.Size = new System.Drawing.Size(181, 20);
+            this.textQR.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Текст для генерации QR:";
+            // 
+            // QRCodeDesktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 275);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textQR);
+            this.Controls.Add(this.pbQR);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnGenerate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "QRCodeDesktop";
+            this.Text = "Генератор QR кодов";
+            ((System.ComponentModel.ISupportInitialize)(this.pbQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pbQR;
+        private System.Windows.Forms.TextBox textQR;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ QRCodeDesktop.cs
@@ -0,0 +1,53 @@
+﻿using QRCoder;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace QRCodeDesktop
+{
+    public partial class QRCodeDesktop : Form
+    {
+        public QRCodeDesktop()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(textQR.Text, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new QRCode(qrCodeData);
+            pbQR.Image = qrCode.GetGraphic(20);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveImage(pbQR.Image);
+        }
+
+        private void SaveImage(Image image)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Filter = "Images|*.jpg;*.png;*.bmp";
+
+            var format = ImageFormat.Jpeg;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(dialog.FileName);
+
+                switch (ext)
+                {
+                    case ".png":
+                        format = ImageFormat.Png;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                image.Save(dialog.FileName, format);
+            }
+        }
+    }
+}
